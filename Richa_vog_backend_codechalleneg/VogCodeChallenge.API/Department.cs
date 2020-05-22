3eb44d62f47a90333b059605216cdfa413b56e9d
Richa Khatri <rc801868@dal.ca>
2020-05-22 19:15:48 -0300

    Added classes and entities

---
+++ Richa_vog_backend_codechalleneg/VogCodeChallenge.API/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API
+{
+    public class Department
+    {
+        public int departmentId { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public IList<Employee> employeeList{get;set;}
+
+    }
+}
---
+++ Richa_vog_backend_codechalleneg/VogCodeChallenge.API/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API
+{
+    public class Employee
+    {
+        public int emplyoeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string jobTitle { get; set; }
+        public string address { get; set; }
+    }
+}
